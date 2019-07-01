319f8a5760637b9ae551d4c11a84f6766dd5fcb0
blaze21777 <stephen.411@hotmail.co.uk>
2019-07-01 18:25:01 +0100

    The source files have arrived.

--- T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/xsim_webtalk.tcl
+++ T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Jul  1 18:12:44 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Jul  1 18:24:27 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2552052" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "7b275c65-65b6-4183-a0f2-a1c14a113268" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "3045efebb63945fcb340d00b565651ef" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz" -context "user_environment"
@@ -26,6 +26,6 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5576_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 542503673 -regid "" -xml D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5636_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1280094278 -regid "" -xml D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/V/VHDL-Projects/T01_HelloWorld/T01_HelloWorld.sim/sim_1/behav/xsim/xsim.dir/T01_HelloWorldTb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
