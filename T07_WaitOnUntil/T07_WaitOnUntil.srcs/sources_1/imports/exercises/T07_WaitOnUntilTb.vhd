84e3d6108eefb58760bfe7f62affd576cc40b587
blaze <stephen.411@hotmail.co.uk>
2019-07-03 09:29:25 +0100

    Working with wait statements.

--- T07_WaitOnUntil/T07_WaitOnUntil.srcs/sources_1/imports/exercises/T07_WaitOnUntilTb.vhd
+++ T07_WaitOnUntil/T07_WaitOnUntil.srcs/sources_1/imports/exercises/T07_WaitOnUntilTb.vhd
@@ -13,7 +13,9 @@ begin
 
         -- TODO: Increment CountUp by 1,
         --       and decrement CountDown by 1 every 10 nanoseconds
-
+        CountUp <= CountUp + 1;
+        CountDown <= CountDown - 1;
+        wait for 10ns;
     end process;
 
     process is
@@ -21,9 +23,10 @@ begin
 
         -- TODO: Use the Wait-On statement to wait until any of the
         --       two signals change
-    
+        wait on CountUp, CountDown;
         -- TODO: Print the values of both of the counter signals
-
+        report "CountUp is: " & integer'image(CountUp) &
+                ", CountDown is: " & integer'image(CountDown);
     end process;
 
     process is
@@ -31,9 +34,9 @@ begin
 
         -- TODO: Use the Wait-Until statement to wait until
         --       the two signals are equal.
-    
+        wait until CountUp = CountDown;
         -- TODO: Print "Jackpot!"
-
+        report "Jackpot!";
     end process;
 
 end architecture;
