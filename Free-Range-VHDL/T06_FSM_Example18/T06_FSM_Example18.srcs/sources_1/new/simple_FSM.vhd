d2e05f82098698da16a0e28e5ff9f700be2e6593
blaze <stephen.411@hotmail.co.uk>
2019-07-14 10:29:52 +0100

    Simple FSM test bench aded.

---
+++ Free-Range-VHDL/T06_FSM_Example18/T06_FSM_Example18.srcs/sources_1/new/simple_FSM.vhd
@@ -0,0 +1,98 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.07.2019 13:55:34
+-- Design Name: 
+-- Module Name: simple_FSM - Behavioral
+-- Project Name: Implementation of a smiple Finite State Machine
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity simple_FSM is
+	port (
+		-- Inputs
+		clk    : in std_logic;
+		clr    : in std_logic;
+		tog_en : in std_logic;
+		-- Outputs
+		Z1     : out std_logic
+	);
+end simple_FSM;
+
+architecture Behavioral of simple_FSM is
+
+	-- The states that can be taken
+	type state is (st0, st1);
+
+	-- State signals
+	signal ps, ns : state;
+
+begin
+	sync_proc : process (clk) is
+	begin
+	    -- Asynchronous reset
+		if (clr = '1') then
+			ps <= st0;
+		elsif rising_edge(clk) then
+			ps <= ns;
+		end if;
+	end process sync_proc;
+
+	next_state_decode : process (tog_en, ps) is
+	begin
+		case ps is
+			when st0 =>
+				-- Stay in current state if not '1'
+				if (tog_en = '1') then
+					ns <= st1;
+				end if;
+
+			when st1 =>
+				-- Stay in current state if not '1'
+				if (tog_en = '1') then
+					ns <= st0;
+				end if;
+			when others =>
+				ns <= st0;
+		end case;
+	end process next_state_decode;
+
+	output_decode : process (ps) is
+	begin
+		Z1 <= '0'; -- Pre-assign output
+		case ps is
+			when st0 =>
+				z1 <= '0'; -- Moore output
+
+			when st1 =>
+				z1 <= '1'; -- Moore output
+
+			when others =>
+				Z1 <= 'X'; -- Error if this is reached
+		end case;
+	end process output_decode;
+
+end Behavioral;
---
+++ Free-Range-VHDL/T06_FSM_Example18/T06_FSM_Example18.srcs/sources_1/new/simple_FSM_tb.vhd
@@ -0,0 +1,82 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.07.2019 09:44:02
+-- Design Name: 
+-- Module Name: simple_FSM_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity simple_FSM_tb is
+	--  Port ( );
+end simple_FSM_tb;
+
+architecture Behavioral of simple_FSM_tb is
+
+	-- Input signals 
+	signal clk          : std_logic;
+	signal clr          : std_logic;
+	signal tog_en       : std_logic;
+	-- Output signals 
+	signal Z1           : std_logic;
+
+	-- Clock signals
+	constant clk_period : time := 10ns;
+	
+begin
+
+	-- FSM instantiation 
+	i_FSM : entity work.simple_FSM(Behavioral) port map (
+		clk    => clk,
+		clr    => clr,
+		tog_en => tog_en,
+		z1     => z1
+		);
+
+	-- Clock process
+	clk_proc : process is
+	begin
+		clk <= '1';
+		wait for clk_period;
+		clk <= '0';
+		wait for clk_period;
+	end process;
+
+	-- Testbench processs
+	test : process is
+	begin
+		wait for 10ns;
+		tog_en <= '1';
+		wait for 10ns;
+		tog_en <= '0';
+		wait for 40ns;
+		clr <= '1';
+		wait for 20ns;
+		clr <= '0';
+
+	end process;
+end Behavioral;
