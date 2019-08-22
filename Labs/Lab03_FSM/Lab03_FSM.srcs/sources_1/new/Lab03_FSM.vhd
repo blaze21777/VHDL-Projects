b938602681b0c14878645f33fb107c1a0ebb7510
blaze <stephen.411@hotmail.co.uk>
2019-08-22 14:31:04 +0100

    Some progress on moore FSM...

--- Labs/Lab03_FSM/Lab03_FSM.srcs/sources_1/new/Lab03_FSM.vhd
+++ Labs/Lab03_FSM/Lab03_FSM.srcs/sources_1/new/Lab03_FSM.vhd
@@ -17,10 +17,8 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,37 +30,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab03_FSM is
-  Port ( 
-    -- Inputs
-    GCLK : in STD_LOGIC;
-    BTNC : in STD_LOGIC;
-    BTNL : in STD_LOGIC;
-    SW0 : in STD_LOGIC;
-    SW1 : in STD_LOGIC;
-    SW2 : in STD_LOGIC;
-    -- Output
-    LD0 : out STD_LOGIC);
+	port (
+		-- Inputs
+		GCLK : in STD_LOGIC;
+		BTNC : in STD_LOGIC; -- Reset
+		BTNL : in STD_LOGIC; -- Switch
+		SW0  : in STD_LOGIC;
+		SW1  : in STD_LOGIC;
+		SW2  : in STD_LOGIC;
+		-- Output
+		LD0  : out STD_LOGIC);
 end Lab03_FSM;
 
 architecture Behavioral of Lab03_FSM is
-    type t_state is (s0, s1, s2, s3, s4);
-    signal state, next_state : t_state;
-    signal reset : std_logic;
-    signal b : std_logic;
-    signal c : std_logic;
-    signal d : std_logic;
-    signal transition : std_logic;
+	type t_state is (s0, s1, s2, s3, s4);
+	signal state, next_state : t_state;
+	signal reset             : std_logic;
+	signal switch            : std_logic;
+	signal output            : std_logic;
+	signal d                 : std_logic;
+	signal transition        : std_logic;
 begin
 
-    sync_proc : process(GCLK,reset) is
-    begin
-        if reset = '1' then  
-        state <= s0;
-        elsif rising_edge(GCLK) then
-        state <= next_state;
-        end if;
-    end process;
-    
-    
+	sync_proc : process (GCLK, reset) is
+	begin
+		if reset = '1' then
+			state <= s0;
+		elsif rising_edge(GCLK) then
+			state <= next_state;
+		end if;
+	end process;
+
+
 
 end Behavioral;
