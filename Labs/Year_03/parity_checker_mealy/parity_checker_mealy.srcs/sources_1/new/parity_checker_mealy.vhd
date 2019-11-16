4c8315240d360ccf8239de5eaf8980f21cff6f8c
blaze <stephen.411@hotmail.co.uk>
2019-11-16 01:59:23 +0000

    Added parity checker mealy.

---
+++ Labs/Year_03/parity_checker_mealy/parity_checker_mealy.srcs/sources_1/new/parity_checker_mealy.vhd
@@ -0,0 +1,97 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.11.2019 01:18:20
+-- Design Name: 
+-- Module Name: parity_checker_mealy - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity parity_checker_mealy is
+    Port ( 
+    clk    : in std_logic;
+		rst    : in std_logic;
+		x      : in std_logic_vector(1 downto 0);
+		parity : out std_Logic
+    );
+end parity_checker_mealy;
+
+architecture Behavioral of parity_checker_mealy is
+
+-- Decalare state that can be taken
+	type state_type is (S0, S1);
+	signal state, next_state : state_type;
+
+begin
+
+sync_proc : process (clk) 
+begin 
+if rising_edge(clk) then 
+    if (rst = '1') then 
+    state <= S0;
+    else 
+    state <= next_state;
+    end if;
+    end if;
+    end process;
+    
+    next_state_decode : process (state, x) 
+    begin 
+     next_state <= S0;
+     case (state) is 
+     when S0 => 
+        if (x = "01") then 
+        next_state <= S1;
+        end if;
+     when S1 =>
+     if (x = "01") then 
+     next_state <= S1;
+     end if;
+     when others => 
+     next_state <= S0;
+     end case;
+    end process;
+    
+    outut_decode : process (state, x) 
+    begin
+     parity <= '0';
+     case (state) is 
+     
+     when S0 =>
+     if (x = "01") then 
+        parity <= '1';
+        end if;
+    when S1 =>
+if (x = "00") then 
+        parity <= '1';
+        end if;
+        when others => 
+        parity <= '0';
+        end case;
+        end process;
+
+end Behavioral;
