c6d74338f46adccdc8e939c7e823b904903262ab
blaze <stephen.411@hotmail.co.uk>
2019-12-11 18:07:47 +0000

    Created basic outline for state machine.

--- Labs/Year_03/threeD_printer/threeD_printer.srcs/sources_1/new/threeD_printer.vhd
+++ Labs/Year_03/threeD_printer/threeD_printer.srcs/sources_1/new/threeD_printer.vhd
@@ -32,12 +32,89 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity threeD_printer is
---  Port ( );
-end threeD_printer;
+  Port ( 
+    -- Inputs
+    cash_en : in std_logic;
+    cash : in std_logic_vector(9 downto 0);
+    cancel : in std_logic;
+    order_en : in std_logic; 
+    order : in std_logic_vector(3 downto 0); 
+    reset : in std_logic; 
+    clock: in std_logic; 
+    -- Outputs
+    check_balance : out std_logic;
+    printing : out std_logic;
+    ready : out std_logic;
+    order_cancelled : out std_logic;
+    change_en : out std_logic;
+    change : out std_logic);
+    end threeD_printer;
 
 architecture Behavioral of threeD_printer is
+    
+    -- Decalare states that can be taken
+    type state_type is (
+        reset_s, 
+        order_s,
+        cancel_s,
+        cash_s,
+        printing_s,
+        ready_s,
+        check_balance_s);
+    signal state, next_state : state_type;
+    
+    -- Outputs as signals 
+    signal check_balance_s : std_logic;  
+    signal printing_s : std_logic;
+    signal ready_s : std_logic;
+    signal order_cancelled_s : std_logic;
+    signal change_en_s : std_logic;
+    signal change_s : std_logic);
 
 begin
+    -- Assigning outputs to signals (change "_s" to "_sig")
+    check_balance_s <=  check_balance;
+    printing_s <= printing;
+    ready_s <=  ready;
+    order_cancelled_s <= order_cancelled;
+    change_en_s <=  change;
+    change_s <=  change;       
 
+    -- The clock process
+    sync_proc : process (clk) 
+begin 
+if rising_edge(clk) then 
+    if (rst = '1') then 
+    state <= S0;
+    else 
+    state <= next_state;
+    end if;
+    end if;
+    end process;
+
+    -- Next state decode 
+    next_state_decode : process (state, order_en, cash_en, change_en, cancel_s) 
+    begin 
+when _s =>
+next_state <= 
+when _s =>
+next_state <= 
+when _s =>
+next_state <= 
+when _s =>
+next_state <= 
+when _s =>
+next_state <= 
+when _s =>
+next_state <= 
+when _s =>
+next_state <= 
+when _s =>
+next_state <=  
+     when others => 
+     next_state <= reset;
+     end case;
+    end process;
+    
 
 end Behavioral;
