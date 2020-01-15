56f01ec6185aee97971172f95e5d6b6d6c3136a0
blaze21777 <stephen.411@hotmail.co.uk>
2020-01-15 11:54:59 +0000

    3D printer formated to be more readable.

--- Labs/Year_03/threeD_printer/threeD_printer.srcs/sources_1/new/threeD_printer.vhd
+++ Labs/Year_03/threeD_printer/threeD_printer.srcs/sources_1/new/threeD_printer.vhd
@@ -1,4 +1,4 @@
-----------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
 -- 
@@ -68,7 +68,7 @@ architecture Behavioral of threeD_printer is
 	-- Order signals
 	signal order_price       : std_logic_vector(num_bits + 2 downto 0) := (others => '0');
 	signal order_save        : std_logic_vector(3 downto 0)            := (others => '0');
-	
+
 	-- Cancel signal
 	signal cancel_save       : std_logic                               := '0';
 
@@ -81,38 +81,38 @@ architecture Behavioral of threeD_printer is
 	signal sub_in1           : std_logic_vector(11 downto 0)           := (others => '0');
 	signal sub_in2           : std_logic_vector(11 downto 0)           := (others => '0');
 	signal sub_out           : std_logic_vector(11 downto 0)           := (others => '0');
-	
+
 	-- Constants as defined in the specfication
 	constant ten_pounds      : std_logic_vector(11 downto 0)           := "001111101000";
 	constant five_pounds     : std_logic_vector(11 downto 0)           := "000111110100";
 	constant two_pounds      : std_logic_vector(11 downto 0)           := "000011001000";
 	constant one_pound       : std_logic_vector(11 downto 0)           := "000001100100";
 	constant fifty_pence     : std_logic_vector(11 downto 0)           := "000000110010";
-	
+
 	-- Counter signals 
 	signal count_rst         : std_logic;
 	signal count_en          : std_logic;
 	signal max_count         : integer;
-	signal count_done              : std_logic;
+	signal count_done        : std_logic;
 begin
 
 	-- Serial adder instatiation
 	serial_adder : entity work.serial_adder(behav)
 		port map(
-			clk   => clk,
-			reset => reset_adder,
-			add_in1  => add_in1,
-			add_in2     => add_in2,
-			add_out     => add_out);
-			
-    -- Counter instantiation 
+			clk     => clk,
+			reset   => reset_adder,
+			add_in1 => add_in1,
+			add_in2 => add_in2,
+			add_out => add_out);
+
+	-- Counter instantiation 
 	counter : entity work.loop_counter(Behavioral)
 		port map(
-			clk       => clk,
-			reset     => count_rst,
-			count_en  => count_en,
-			max_count => max_count,
-			count_done      => count_done);
+			clk        => clk,
+			reset      => count_rst,
+			count_en   => count_en,
+			max_count  => max_count,
+			count_done => count_done);
 
 	-- The clock process
 	sync_proc : process (clk)
@@ -125,22 +125,22 @@ begin
 			end if;
 		end if;
 	end process;
-	
-    ----------------------------------------------------------
+
+	----------------------------------------------------------
 	------------------- Next state decode -------------------- 
 	----------------------------------------------------------
-	next_state_decode : process (order_price, sub_out, cancel, 
-	                             state, order_en, cash_en, 
-	                             count_done,add_out, cancel_save) is 
+	next_state_decode : process (order_price, sub_out, cancel,
+		state, order_en, cash_en,
+		count_done, add_out, cancel_save) is
 
 	begin
 
 		case state is
-		    ------------------- Reset state ---------------------- 
-		    when reset_s => 
-		    next_state <= add_s;
-		    
-			------------------- Add state ------------------------ 
+				------------------- Reset state ---------------------- 
+			when reset_s =>
+				next_state <= add_s;
+
+				------------------- Add state ------------------------ 
 			when add_s =>
 				if (order_en = '1') then
 					next_state <= pre_order_s;
@@ -148,7 +148,7 @@ begin
 					next_state <= cancel_s;
 				end if;
 
-			------------------- Pre order state -------------------
+				------------------- Pre order state -------------------
 			when pre_order_s =>
 				if (cancel = '1') then -- 
 					next_state <= cancel_s;
@@ -156,7 +156,7 @@ begin
 					next_state <= order_s;
 				end if;
 
-			------------------- Order state ------------------------
+				------------------- Order state ------------------------
 			when order_s =>
 				-- If balance => order price, then execute the order and give the change back
 				-- If balance =< order price, then cancel the order and give the current balance back
@@ -168,68 +168,68 @@ begin
 					next_state <= subtract_s;
 				end if;
 
-			-------------------  Subtract state ---------------------
+				-------------------  Subtract state ---------------------
 			when subtract_s =>
 				next_state <= change_s;
 
-			------------------- Change state ------------------------
+				------------------- Change state ------------------------
 			when change_s =>
-							
+
 				if (sub_out > ten_pounds) then -- Greather than £10
-					next_state <= subtract_s;
+					next_state  <= subtract_s;
 				elsif (sub_out > five_pounds and sub_out <= ten_pounds) then -- Greater than £5
-					next_state <= subtract_s;
+					next_state  <= subtract_s;
 				elsif (sub_out > two_pounds and sub_out <= five_pounds) then -- Greater than £2
-					next_state <= subtract_s;
+					next_state  <= subtract_s;
 				elsif (sub_out > one_pound and sub_out <= two_pounds) then -- Greater than £1
 					next_state <= subtract_s;
 				elsif (sub_out > "000000000000" and sub_out <= one_pound) then -- Greater than 50p
 					next_state <= subtract_s;
 				elsif (sub_out = "000000000000") then -- Greater than 50p
-				    if (cancel_save = '1') then 
-				    next_state <= reset_s;
-				    else	
-				    next_state <= printing_s;
-				    end if;
+					if (cancel_save = '1') then
+						next_state <= reset_s;
+					else
+						next_state <= printing_s;
+					end if;
 				end if;
 
-			------------------- Cancel state -------------------------
+				------------------- Cancel state -------------------------
 			when cancel_s =>
 
 				next_state <= subtract_s;
 
-			------------------- Printing state -----------------------
+				------------------- Printing state -----------------------
 			when printing_s =>
-			    -- done = delay signal to simulate printing
-			    -- Extra cycle is taken to change state but printing is finished when done = 1
-				if (count_done = '1') then 
+				-- done = delay signal to simulate printing
+				-- Extra cycle is taken to change state but printing is finished when done = 1
+				if (count_done = '1') then
 					next_state <= ready_s;
 				end if;
-			
-			------------------- Ready state --------------------------
+
+				------------------- Ready state --------------------------
 			when ready_s =>
-			next_state <= reset_s;
-			
-            ------------------- Check balance state ------------------
+				next_state <= reset_s;
+
+				------------------- Check balance state ------------------
 			when check_balance_s =>
-            
-            ------------------- Error check state --------------------
-            -- FSM should neve make it to this state if error free
+
+				------------------- Error check state --------------------
+				-- FSM should neve make it to this state if error free
 			when others          =>
 				next_state <= reset_s;
 		end case;
 	end process;
-	
-    --------------------------------------------------------- 
+
+	--------------------------------------------------------- 
 	------------------- Output decode -----------------------
 	--------------------------------------------------------- 
-	output_decode : process (add_out, sub_in1, sub_in2, sub_out, 
-	                         order, order_en, state, order_save,
-	                         cash, cash_en, order_price) is
+	output_decode : process (add_out, sub_in1, sub_in2, sub_out,
+		order, order_en, state, order_save,
+		cash, cash_en, order_price) is
 	begin
 		case state is
 
-			------------------- Reset state --------------------- 
+				------------------- Reset state --------------------- 
 			when reset_s =>
 				-- Reset FSM outputs and signal 
 				check_balance   <= '0';
@@ -238,42 +238,42 @@ begin
 				order_cancelled <= '0';
 				change_en       <= '0';
 				change          <= "0000000000";
-				
+
 				-- Reset buffer signals
-                cancel_save <= '0';
-                order_cancelled <= '0';
-               -- order_save <= "0000";
-                
+				cancel_save     <= '0';
+				order_cancelled <= '0';
+				-- order_save <= "0000";
+
 				-- Counter signals 
 				count_rst       <= '1'; -- Reset counter 
 				count_en        <= '0';
 				max_count       <= 0;
-      
+
 				-- Adder signals 
 				reset_adder     <= '0'; -- Reset in ready state
-				
-			------------------- Add state -----------------------
-			check_balance <= '1'; -- When 1 adder output will be displayed to a display 
+
+				------------------- Add state -----------------------
+				check_balance   <= '1'; -- When 1 adder output will be displayed to a display 
 			when add_s =>
 				-- Adding takes place in reset as no need for extra state.
 				if (cash_en = '1') then
-					add_in1    <= "00" & cash; -- 12-bit assignment 
-					add_in2    <= "000000000000"; -- Always zero as after first assignment output is used 
+					add_in1 <= "00" & cash;    -- 12-bit assignment 
+					add_in2 <= "000000000000"; -- Always zero as after first assignment output is used 
 				else
 					add_in1 <= "000000000000"; -- Stop adding when cash_en is off
 
 				end if;
-                
-                -- Next state condition 
+
+				-- Next state condition 
 				if (order_en = '1') then
 					order_save <= order; -- Save order to be used later 
 				end if;
 
-		    ------------------- Pre order state ------------------- 
-		    -- Order prices are assigned in this state 
+				------------------- Pre order state ------------------- 
+				-- Order prices are assigned in this state 
 			when pre_order_s =>
 				case order_save is -- Uses order save as order gets reset 
-					when "0000" => -- No order
+					when "0000" =>     -- No order
 						order_price <= x"000";
 
 					when "0001" => -- Gun
@@ -325,132 +325,130 @@ begin
 						order_price <= x"000";
 				end case;
 
-			------------------- Order state -----------------------
+				------------------- Order state -----------------------
 			when order_s =>
-                -- Assign values in preperation for change calculation 
-				sub_in1 <= add_out;           
+				-- Assign values in preperation for change calculation 
+				sub_in1 <= add_out;
 				sub_in2 <= order_price; -- If bigger than s order is cancelled so no negative numbers 
 
-			------------------- Subtract state --------------------
+				------------------- Subtract state --------------------
 			when subtract_s =>
-			    -- Subtraction calculation 
+				-- Subtraction calculation 
 				sub_out <= std_logic_vector(unsigned(sub_in1) - unsigned(sub_in2));
 
-			------------------- Change state ----------------------     
-			-- Change calculation takes multiple clock cycles as total cash needs to be 
-			-- split into 10-bit values of accepted cash                                                                                                
+				------------------- Change state ----------------------     
+				-- Change calculation takes multiple clock cycles as total cash needs to be 
+				-- split into 10-bit values of accepted cash                                                                                                
 			when change_s =>
-				change_en <= '1'; -- Turn on when change is being calculated 
-				count_rst <= '0'; -- Allows counter to count
-				
+				change_en <= '1';              -- Turn on when change is being calculated 
+				count_rst <= '0';              -- Allows counter to count
+
 				-- Keep subtracting until zero is reached
 				if (sub_out > ten_pounds) then -- Greater than £10 
 					change  <= ten_pounds(num_bits downto 0);
 					sub_in1 <= sub_out;
 					sub_in2 <= ten_pounds;
-					
-				elsif (sub_out >= five_pounds and sub_out <= ten_pounds) then -- Greater than £5
+
+				elsif (sub_out >= five_pounds and sub_out   <= ten_pounds) then -- Greater than £5
 					change  <= five_pounds(num_bits downto 0);
 					sub_in1 <= sub_out;
 					sub_in2 <= five_pounds;
-					
-				elsif (sub_out >= two_pounds and sub_out <= five_pounds) then -- Greater than £2
+
+				elsif (sub_out >= two_pounds and sub_out    <= five_pounds) then -- Greater than £2
 					change  <= two_pounds(num_bits downto 0);
 					sub_in1 <= sub_out;
 					sub_in2 <= two_pounds;
-					
-				elsif (sub_out >= one_pound and sub_out <= two_pounds) then -- Greater than £1
+
+				elsif (sub_out >= one_pound and sub_out     <= two_pounds) then -- Greater than £1
 					change  <= one_pound(num_bits downto 0);
 					sub_in1 <= sub_out;
 					sub_in2 <= one_pound;
-					
+
 				elsif (sub_out > "000000000000" and sub_out <= one_pound) then -- Greater than 50p
-				    change  <= fifty_pence(num_bits downto 0);
+					change  <= fifty_pence(num_bits downto 0);
 					sub_in1 <= sub_out;
 					sub_in2 <= fifty_pence;
-					
-				elsif (sub_out = "000000000000") then -- Zero
-				change  <= "0000000000"; -- Must set to zero as it keeps last value for infinte change despensing
-				change_en <= '0'; -- Change is no longer being calculated 
-				end if;
-				
 
-			------------------- Cancel state ----------------------
-			when cancel_s   =>
-				sub_in1 <= add_out;           
-				sub_in2 <= "000000000000"; -- An intial value is required 
-                cancel_save <= '1'; -- Indicate cancel has been triggered 
-                order_cancelled <= '1'; -- Order has been cancelled (can't read ouputs)
-                reset_adder <= '1';
-                
-			------------------- Printing state ---------------------
+				elsif (sub_out = "000000000000") then -- Zero
+					change    <= "0000000000";            -- Must set to zero as it keeps last value for infinte change despensing
+					change_en <= '0';                     -- Change is no longer being calculated 
+				end if;
+				------------------- Cancel state ----------------------
+			when cancel_s =>
+				sub_in1         <= add_out;
+				sub_in2         <= "000000000000"; -- An intial value is required 
+				cancel_save     <= '1';            -- Indicate cancel has been triggered 
+				order_cancelled <= '1';            -- Order has been cancelled (can't read ouputs)
+				reset_adder     <= '1';
+
+				------------------- Printing state ---------------------
 			when printing_s =>
-			      -- Check balance cannot happen at the same time as printing 
-			      -- Balance goes to zero once change has been given
-			      check_balance <= '0'; 
-				  printing <= '1'; -- Printing in progress
+				-- Check balance cannot happen at the same time as printing 
+				-- Balance goes to zero once change has been given
+				check_balance <= '0';
+				printing      <= '1'; -- Printing in progress
 				case order_save is
 					when "0000" => -- No order
-                        -- NEED TO FIGURE OUT WHAT HAPPENS HERE
-                        
+						-- NEED TO FIGURE OUT WHAT HAPPENS HERE
+
 					when "0001" => -- Gun
-						max_count  <= 1;
-						count_en   <= '1';
+						max_count <= 1;
+						count_en  <= '1';
 
 					when "0010" => -- Capes or cloaks
-						max_count  <= 2;
-						count_en   <= '1';
+						max_count <= 2;
+						count_en  <= '1';
 
 					when "0011" => -- Gun + cape or cloak
-						max_count  <= 3;
-						count_en   <= '1';
+						max_count <= 3;
+						count_en  <= '1';
 
 					when "0100" => -- Yoda
-						max_count  <= 4;
-						count_en   <= '1';
+						max_count <= 4;
+						count_en  <= '1';
 
 					when "0101" => -- Yoda + lightsaber
-						max_count  <= 5;
-						count_en   <= '1';
+						max_count <= 5;
+						count_en  <= '1';
 					when "0110" => -- Yoda + cloak
-						max_count  <= 6;
-						count_en   <= '1';
+						max_count <= 6;
+						count_en  <= '1';
 
 					when "0111" => -- Yoda + cloak + lightsaber
-						max_count  <= 7;
-						count_en   <= '1';
+						max_count <= 7;
+						count_en  <= '1';
 
 					when "1000" => -- Leia
-						max_count  <= 8;
-						count_en   <= '1';
+						max_count <= 8;
+						count_en  <= '1';
 
 					when "1001" => -- Leia + rifle
-						max_count  <= 9;
-						count_en   <= '1';
+						max_count <= 9;
+						count_en  <= '1';
 
 					when "1010" => -- Leia + cape
-						max_count  <= 10;
-						count_en   <= '1';
+						max_count <= 10;
+						count_en  <= '1';
 
 					when "1011" => -- Leia + cloak + lightsaber
-						max_count  <= 11;
-						count_en   <= '1';
+						max_count <= 11;
+						count_en  <= '1';
 
 					when "1100" => -- Darth Vader
-						max_count  <= 12;
-						count_en   <= '1';
+						max_count <= 12;
+						count_en  <= '1';
 
 					when "1101" => -- Darth Vader + lightsaber
-						max_count  <= 13;
-						count_en   <= '1';
+						max_count <= 13;
+						count_en  <= '1';
 
 					when "1110" => -- Darth Vader + cloak
-						max_count  <= 14;
-						count_en   <= '1';
+						max_count <= 14;
+						count_en  <= '1';
 
 					when "1111" => -- Darth Vader + lightsaber + cloak
-						max_count  <= 15;
-						count_en   <= '1';
+						max_count <= 15;
+						count_en  <= '1';
 
 					when others => -- Should never make it here
 
