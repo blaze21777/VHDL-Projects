5df7a9fbffc9b339e5c4878e535c54d5107a1b3a
blaze21777 <stephen.411@hotmail.co.uk>
2020-01-15 11:33:37 +0000

    Made adder inputs more readable.

--- Labs/Year_03/threeD_printer/threeD_printer.srcs/sources_1/imports/new/serial_adder.vhd
+++ Labs/Year_03/threeD_printer/threeD_printer.srcs/sources_1/imports/new/serial_adder.vhd
@@ -31,45 +31,42 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
---serial adder for N bits. Note that we dont have to mention N here.
+-- Serial adder adapted from
 -- https://vhdlguru.blogspot.com/2017/11/vhdl-code-for-n-bit-serial-adder-with.html 
 entity serial_adder is
     generic (num_bits : integer := 11);
     port(Clk,reset : in std_logic; --clock and reset signal
-          --  cin : in std_logic;  --note that cin is used for only first iteration.
-            a,b : in std_logic_vector(num_bits downto 0);
-            s : out std_logic_vector(num_bits downto 0)  --note that s comes out at every clock cycle and cout is valid only for last clock cycle.
+            add_in1, add_in2 : in std_logic_vector(num_bits downto 0);
+            -- note that add_out comes out at every clock cycle
+            add_out : out std_logic_vector(num_bits downto 0)  
             );
 end serial_adder;
 
 architecture behav of serial_adder is
 
---intermediate signals.
+-- Intermediate signals.
 signal flag : std_logic := '0';
 signal s_buf : std_logic_vector(num_bits downto 0);
 
 begin
 
 process(clk,reset)
---we use variable, so that we need the carry value to be updated immediately.
+-- We use a variable so that the buffer value is updated immediately.
 variable s_buf : std_logic_vector(num_bits downto 0) := (OTHERS => '0'); 
 begin
-if(reset = '1') then --active high reset
+if(reset = '1') then -- Active high reset
     s_buf := "000000000000";
-    --cout <= c;
     flag <= '0';
 elsif(rising_edge(clk)) then
     if(flag = '0') then
-        s_buf := std_logic_vector(unsigned(a) + unsigned(b));  --on first iteration after reset, assign cin to c.
-        flag <= '1';  --then make flag 1, so that this if statement isnt executed any more.
+        s_buf := std_logic_vector(unsigned(add_in1) + unsigned(add_in2));  
+        flag <= '1';  -- Make flag 1 so that this if statement isnt executed any more.
     else 
-    -- cout <= '0'; 
-    s_buf := std_logic_vector(unsigned(a) + unsigned(s_buf));  --SUM
-    -- c := (a and b) or (c and b) or (a and c);  --CARRY
+    s_buf := std_logic_vector(unsigned(add_in1) + unsigned(s_buf));  --SUM
     end if; 
    
 end if;
-s <= s_buf;
+add_out <= s_buf;
 end process;
 
 end behav;
