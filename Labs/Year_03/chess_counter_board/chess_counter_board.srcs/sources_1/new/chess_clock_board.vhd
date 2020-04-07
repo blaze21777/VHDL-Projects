06096253728d3b8732116cb06b0a226d355a1232
blaze21777 <stephen.411@hotmail.co.uk>
2020-04-07 12:58:47 +0100

    Chess counter start.

--- Labs/Year_03/chess_counter_board/chess_counter_board.srcs/sources_1/new/chess_clock_board.vhd
+++ Labs/Year_03/chess_counter_board/chess_counter_board.srcs/sources_1/new/chess_clock_board.vhd
@@ -32,12 +32,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity chess_clock_board is
---  Port ( );
+  Port (
+    SW0 : in STD_LOGIC;
+    SW1 : in STD_LOGIC;
+    SW2 : in STD_LOGIC;
+    SW3 : in STD_LOGIC;
+    SW4 : in STD_LOGIC;
+    SW5 : in STD_LOGIC;
+    SW6 : in STD_LOGIC;
+    SW7 : in STD_LOGIC;
+    BTNC : in STD_LOGIC; -- Allow input greater than 8 switches
+    BTNL : in STD_LOGIC; -- Reset button?
+    BTNR : in STD_LOGIC; -- Button for wrong mate 
+    LD0 : out STD_LOGIC; -- Can use LEDs to represent which part of the value is being taken
+    LD1 : out STD_LOGIC;
+    LD2 : out STD_LOGIC;
+    LD3 : out STD_LOGIC;
+    LD4 : out STD_LOGIC;
+    LD5 : out STD_LOGIC;
+    LD6 : out STD_LOGIC;
+    LD7 : out STD_LOGIC
+   
+   );
 end chess_clock_board;
 
 architecture Behavioral of chess_clock_board is
 
 begin
+
+-- Instantiate chess clock 
+    -- chess clock will use 4 digit decimal counter 
+    -- decimal counters will be created with generate 
 
 
 end Behavioral;
