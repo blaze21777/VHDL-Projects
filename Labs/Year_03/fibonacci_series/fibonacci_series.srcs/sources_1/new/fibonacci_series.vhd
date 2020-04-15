56dc716e710a791f82e069e4c141b3ffbbab69f9
blaze21777 <stephen.411@hotmail.co.uk>
2020-04-15 17:31:56 +0100

    Fibonacci series intial.

---
+++ Labs/Year_03/fibonacci_series/fibonacci_series.srcs/sources_1/new/fibonacci_series.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 15.04.2020 17:19:56
+-- Design Name: 
+-- Module Name: fibonacci_series - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity fibonacci_series is
+	generic (N : integer := 16); ------number of bits
+	port (
+		clk, rst    : in bit;
+		fibo_series : out integer range 0 to 2 ** N - 1);
+end fibonacci_series;
+
+architecture Behavioral of fibonacci_series is
+	signal a, b, c : integer range 0 to 2 ** N - 1;
+begin
+	process (clk, rst)
+	begin
+		if (rst = '1') then
+			b <= 1;
+			c <= 0;
+		elsif (clk'event and clk = '1') then
+			c <= b;
+			b <= a;
+		end if;
+		a <= b + c;
+	end process;
+	fibo_series <= c;
+end Behavioral;
