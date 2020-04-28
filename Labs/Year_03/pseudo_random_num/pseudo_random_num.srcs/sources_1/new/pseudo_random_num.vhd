ba1815a9eeb5d57c3ffecc006c1602ce5b2d7104
blaze21777 <stephen.411@hotmail.co.uk>
2020-04-28 18:28:38 +0100

    Started PRNG code layout.

--- Labs/Year_03/pseudo_random_num/pseudo_random_num.srcs/sources_1/new/pseudo_random_num.vhd
+++ Labs/Year_03/pseudo_random_num/pseudo_random_num.srcs/sources_1/new/pseudo_random_num.vhd
@@ -32,12 +32,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity pseudo_random_num is
---  Port ( );
+  Port (
+    -- Inputs
+    clk : in std_logic;
+    reset : in std_logic;
+    -- Outputs
+    number : out std_logic_vector(31 downto 0)  -- Has to be 32-bit output
+   );
 end pseudo_random_num;
 
 architecture Behavioral of pseudo_random_num is
 
 begin
 
+    -- Fibonacci sequence1 instatiation
+--	 : entity work.serial_adder(behav)
+--		port map(
+--			clk     => clk,
+--			reset   => reset_adder,
 
+    -- Fibonacci sequence2
+    
+    -- PRNG algorithm A instatiation
+    
+    -- PRNG algorithm B instatiation
+    
+    
 end Behavioral;
