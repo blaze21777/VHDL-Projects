eb5a19dac7ef02ae6fabd6c54f5ab51d5afe962a
blaze21777 <stephen.411@hotmail.co.uk>
2020-05-06 14:57:42 +0100

    Created sorting modulde file.

---
+++ Labs/Year_03/sorting_module/sorting_module.srcs/sources_1/new/sorting_module.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.05.2020 14:38:25
+-- Design Name: 
+-- Module Name: sorting_module - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sorting_module is
+--  Port ( );
+end sorting_module;
+
+architecture Behavioral of sorting_module is
+
+begin
+
+
+end Behavioral;
